f36b95bddec402ebbfedd9c77df2c1b22ea5cc91
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2020-11-23 14:58:59 +0500

    Create Customportscanner.py
    Requirements:
    
    Python3

---
+++ Customportscanner.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
+sys.ps1 = '\033[01;32m '
+print(sys.ps1)
+print('''
+
+ ####### ######   #####   #####  
+    #    #     # #     # #     # 
+    #    #     #       #       # 
+    #    ######   #####   #####  
+    #    #   #         #       # 
+    #    #    #  #     # #     # 
+    #    #     #  #####   #####  
+                                 
+
+''')
+
+
+
+target = input("Enter The IP: ")
+
+port = int(input("Enter the port you wanna scan: "))
+
+
+def scan(port):
+    if s.connect_ex((target, port)):
+        print(port, "port is closed")
+    else:
+        print(port, " port is open")
+    
+
+scan(port)
+             
